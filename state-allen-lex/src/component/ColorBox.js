5a3d09dc15fe1beb57a4fd225c1f7ebc8bceb190
algtan <agtan@ymail.com>
2021-02-19 12:00:52 -0800

    handleClick function created

--- state-allen-lex/src/component/ColorBox.js
+++ state-allen-lex/src/component/ColorBox.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 
 class ColorBox extends Component {
 
+  handleClick() {
+    console.log('The box was clicked');
+    // Use a random number generator
+    // Assign colors to the numbers
+    // Return a random color
+  }
+
   render() {
     return (
       <>
-        <div className="rectangle"> </div>
+        <div className="rectangle" onClick={this.handleClick}> </div>
+        
       </>
     )
   }
