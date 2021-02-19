0a2b66fb5721002978b0926023de141255f4b9fb
LPeterson18 <lpeterson@chorus.net>
2021-02-19 11:32:11 -0800

    made a block

---
+++ state-allen-lex/src/component/ColorBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ColorBox extends Component {
+
+  render() {
+    return (
+      <>
+        <div className="rectangle"> </div>
+      </>
+    )
+  }
+}
+export default ColorBox;
