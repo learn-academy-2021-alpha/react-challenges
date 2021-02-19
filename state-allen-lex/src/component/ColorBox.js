b22ccdd4ee9b97472d91af8d200e2015823fb5e7
LPeterson18 <lpeterson@chorus.net>
2021-02-19 12:13:29 -0800

    text appears in box

--- state-allen-lex/src/component/ColorBox.js
+++ state-allen-lex/src/component/ColorBox.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react'
 
 class ColorBox extends Component {
 
-  handleClick() {
+constructor(props){
+  super(props)
+  this.state = {
+    color: ""
+  }
+}
+
+
+  handleClick = () =>{
     console.log('The box was clicked');
     // Use a random number generator
     // Assign colors to the numbers
     // Return a random color
+    this.setState({color:"color"})
   }
 
   render() {
     return (
       <>
-        <div className="rectangle" onClick={this.handleClick}> </div>
-        
+        <div className="rectangle" onClick={this.handleClick}> {this.state.color} </div>
       </>
     )
   }
