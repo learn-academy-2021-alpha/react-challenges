96182b7bd9181584d4ff3d3a4d13fbe6adb36f5c
romerofernando <romero_f22@yahoo.com>
2021-02-19 11:32:18 -0800

    first commit

---
+++ react-challenges-deven-fernando/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import Colors from './components/colors'
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.State = {
+
+    }
+  }
+  render(){
+    <>
+      <div style="width:500px;height:100px;border:1px solid #000;">
+        
+  
+      </div> 
+      
+     
+
+   </>
+  }
+}
+
+
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ react-challenges-deven-fernando/src/components/colors.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Colors extends Component { 
+    constructor(props){
+        super(props)
+        this.State = {
+            color: "white",
+
+    }
+}
+}
+
+export default Colors;
