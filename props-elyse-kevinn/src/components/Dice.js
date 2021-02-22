ba067b5d3f2ccd20bd7eac774915e6e881f1e96d
kevinnarbas <kevinnarbas@gmail.com>
2021-02-22 15:03:34 -0800

    created new react app and dice and rolls components

---
+++ props-elyse-kevinn/src/components/Dice.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+
+class Dice extends Component {
+  constructor() {
+    super()
+    this.state = {
+      randomDiceNum = undefined
+    }
+  }
+
+  getRandomDice = () => {
+    const newDice = this.props.
+  }
+  
+  render() {
+    return (
+      <>
+        <h1>Dice</h1>
+      </>
+    )
+  }
+}
+
+
+
+export default Dice;
---
+++ props-elyse-kevinn/src/components/Rolls.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+
+class Rolls extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  
+  render() {
+    return (
+      <>
+        <h1>Rolls</h1>
+      </>
+    )
+  }
+}
+
+
+
+export default Rolls;
