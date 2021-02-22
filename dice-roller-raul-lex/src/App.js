2375f99017f7c9dfa21903d90b45639a6508d6e0
REV004 <rsier004@gmail.com>
2021-02-22 15:00:48 -0800

    finish

---
+++ dice-roller-raul-lex/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Dice from "./components/dice";
+import Log from "./components/log";
+
+class App extends Component {
+	render() {
+		return <></>;
+	}
+}
+
+export default App;
---
+++ dice-roller-raul-lex/src/components/dice.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Dice extends Component {
+	constructor (props)
+    
+    render() {
+		return <></>;
+	}
+}
+
+export default Dice;
