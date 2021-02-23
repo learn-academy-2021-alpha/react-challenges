d8ec8fefd8fd3cc2eafb8b0c2ac9e757b081f7d9
LEARN Academy <>
2021-02-22 16:11:51 -0800

    figured out some more logic on both the app.js and rollthedice.js

--- props-deven-angelo/src/App.js
+++ props-deven-angelo/src/App.js
@@ -1,26 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import RollTheDice from "./components/diceRoller";
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(){
+    super();
     this.state = {
-      diceRoll: ['1' ,'2' ,'3' ,'4' ,'5' ,'6'] , 
+      diceRoll: ['1' ,'2' ,'3' ,'4' ,'5' ,'6'] ,
       currentRoll: 0
     }
   }
   rollingDice = () => {
     let throwDice = Math.floor(Math.random() * this.state.diceRoll.length)
     this.setState({ currentRoll: throwDice })
-  }
-}
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  };
 }
 
+render() {
+  return(
+    <div className="App">
+    roll the dice!
+    {
+      this.state.diceRoll[this.state.currentRoll.map(diceRoll, index) => {
+        return < RollTheDice number = {diceRoll}
+                            key = {index}
+                            diceAction = {this.rollingDice}/>
+      }]
+    }
+    </div>
+    );
+  }
+}
 export default App
 
 //create a function that will display an array of 6 random numbers and spit out those numbers
