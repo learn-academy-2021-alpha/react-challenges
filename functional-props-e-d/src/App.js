bb031ac6ae65c571689297aa5a06ee87d4e06a78
DevenPerkins <deven.s.perkins@gmail.com>
2021-02-24 11:12:45 -0800

    three files and lologic created

--- functional-props-e-d/src/App.js
+++ functional-props-e-d/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import './components/Items'
+import './components/Cart'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      food: [ 'fish - $5 lb' , 'broccoli - 99c', 'berries - $2.99 pt' , 'english muffins $3' , 'yogurt $2' ],
+      household: ['paper towels $10' , 'toilet paper $25' , 'toothpaste $1' , 'deodorant $2' ,'body wash $4'],
+      cart: []
+    }
+  }
+addItemsToCart = (newItem) => {
+  this.setState({ cart: [...this.state.cart, newItem] })
 }
 
-export default App;
+render(){
+  return(
+    <>
+    <h1>Grocery App</h1>
+    <h3>Please Select Food Items</h3>
+    <h3>Please Select Household Items</h3>
+    </>
+  )
+ }
+}
+export default App
---
+++ functional-props-e-d/src/components/Cart.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+
+class Cart extends Component {
+render(){
+    return(
+      <>
+      
+      </>
+    )
+   }
+  }
+
+export default Cart
