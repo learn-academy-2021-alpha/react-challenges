1d588dc23a115ea3613573f2fca678db231d3557
nickdsaunders <nickdavidsaunders@gmail.com>
2021-03-01 14:34:42 -0800

    changes to challenges

--- props-allen-nick/src/App.js
+++ props-allen-nick/src/App.js
@@ -14,18 +14,26 @@ class App extends Component{
   }
 
   rollDice = () => {
-    let randomNum = Math.ceil(Math.random() * 6)
-    this.setState({ result: randomNum })
-    let currentLog = this.state.log
-    currentLog.push(randomNum)
-    this.setState({log: currentLog})
+    // let randomNum = Math.ceil(Math.random() * 6)
+    // this.setState({ result: randomNum })
+    // let currentLog = this.state.log
+    // currentLog.push(randomNum)
+    // this.setState({log: currentLog})
+    let dice = Math.ceil(Math.random() * 6)
+    this.setState({
+      result: dice,
+      log: [...this.state.log, dice]
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h1>Dice Roller</h1>
-        <Dice rollFunction={this.rollDice} rollResult={this.state.result} />
+        <Dice 
+        rollFunction={this.rollDice} 
+        rollResult={this.state.result} 
+        />
         <Rolls logOfResults={this.state.log} />
       </div>
     );
