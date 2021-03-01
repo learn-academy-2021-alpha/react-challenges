874df36143a35d9284e81e20aecfc7954c6725f7
justinhilario <justinhilario@gmail.com>
2021-03-01 15:02:13 -0800

    bla

--- inputs-justin-lex/src/App.js
+++ inputs-justin-lex/src/App.js
@@ -13,13 +13,15 @@ class App extends Component {
 
 
   handleChange = (e) => {
-    console.log(e);
-    console.log(e.target);
-    console.log(e.target.value);
-
-
+    // console.log(e);
+    // console.log(e.target);
+    // console.log(e.target.value);
+    
+    let bad = e.target.value;
+    bad = bad.replace(/./g,"bla") 
+    console.log(bad)
     this.setState({ userInput: e.target.value,
-                    badInput: e.target.value,
+                    badInput:bad,
                     kanyeInput: e.target.value })
   }
 
@@ -39,14 +41,15 @@ class App extends Component {
         <h1>Listening Robot</h1>
         <input
           type="text"
+          value={this.state.userInput}
           onChange={ this.handleChange }
         />
         <h3>Good Robot</h3>
         <p> I hear you saying { this.state.userInput }. Is that correct? </p>
         <h3>Bad Robot</h3>
-        <p> { this.state.badInput } </p>
-        <h3>Kanyebot</h3>
-        <p> Imma let you finish, but Beyonce is { this.state.kanyeInput } </p>
+        <p> I hear you saying { this.state.badInput}. Is that correct? </p>
+        <h3>Learn bot</h3>
+        <p> Imma let you finish, but Learn Academy is { this.state.userInput } </p>
       </>
     )
   }
