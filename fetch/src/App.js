0e78acbaa8bfa7a744e8cc50d48f6e5907c37777
DevenPerkins <deven.s.perkins@gmail.com>
2021-03-29 15:45:05 -0700

    we were almost there

--- fetch/src/App.js
+++ fetch/src/App.js
@@ -4,28 +4,35 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      ipa: ""
+      nasa: null,
+      pic: null
     }
   }
 
 getIPA = () => {
-  fetch('https://ipapi.co/json/')
+  fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
   .then(banana => {
     return banana.json()
   })
   .then(payload => {
-    this.setState({ ipa: payload })
+    this.setState({ nasa: payload })
   })
   .catch(error => {
     console.log(error);
   })
 }
+getPic = () => {
+  let randomNum = Math.floor((Math.random) * this.state.nasa.length)
+  this.setState({ pic : this.state.nasa[randomNum].img_src})
 
+
+} 
   render(){
     return(
       <>
       <h1>My IPA -- hold my beer.</h1>
-      
+      <button onClick={ this.pic }> Choose Beer </button>
+      <p>{ this.state.nasa.id}</p>
       </>
     )
   }
