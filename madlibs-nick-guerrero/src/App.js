7d6ff180ab1a45ec6d7e0a4537b4ddcd95c506d4
guerrero <jrheezy.gc@gmail.com>
2021-03-29 15:11:04 -0700

    done

--- madlibs-nick-guerrero/src/App.js
+++ madlibs-nick-guerrero/src/App.js
@@ -22,11 +22,10 @@ class App extends Component {
       <h1>Mad Libs</h1>
       <h3>Text Type</h3>
         <TextInput type={this.state.textType}/>
-        <button onClick= { () => 
-        this.state.addTextToInput() }>Submit</button>
+        <button onClick = { this.addTextToInput }>Submit</button>
         <button >Clear</button>
         <p>
-        Once upon a time, there was a silly old baboon named {}. He decided he wanted a nice big {} to eat. So he got in his {} and drove it to the nearest {} store. But he had no money to pay the {} store owner. So he said he would help out around the store by {}. He did such a good job at {} that he got another {} free.
+        Once upon a time, there was a silly old baboon named {this.state.addedToMadLibs}. He decided he wanted a nice big {} to eat. So he got in his {} and drove it to the nearest {} store. But he had no money to pay the {} store owner. So he said he would help out around the store by {}. He did such a good job at {} that he got another {} free.
         </p>
       </>
     )
